97c289d68e3d29947b570c52434e4ac86e07360e
Dibyajyoti Chakrabarti <dibyajyoti.chakrabarti.122@gmail.com>
2025-10-18 23:34:25 +0530

    Added Basic Routing 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Landing from './components/Landing'
+import Signin from './components/Signin'
+import Login from './components/Login'
+import Track from './components/Track'
+import Report from './components/Report'
+import Profile from './components/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <div className='bg-red-600 font-bold text-white'>Hi</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="/Login" element={<Login/>}/>
+        <Route path="/Signin" element={<Signin/>}/>
+        <Route path="/Track" element={<Track/>}/>
+        <Route path="/Report" element={<Report/>}/>
+        <Route path="/Profile" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
