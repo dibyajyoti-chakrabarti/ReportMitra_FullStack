342b6113fbac33ed7beef4de6b6f68042a7f0ebf
Dibyajyoti Chakrabarti <dibyajyoti.chakrabarti.122@gmail.com>
2025-10-22 16:07:36 +0530

    Created Track-2; with Report Actions

--- frontend/src/components/IssueAction.jsx
+++ frontend/src/components/IssueAction.jsx
@@ -1,8 +1,40 @@
-function IssueAction(){
-    return (
-        <div>
-            Hello from Issue Action
+function IssueAction() {
+    const trackingID='DEFAULT_VAL';
+    const status='DEFAULT_VAL'
+    const dept='DEFAULT_VAL'
+    const ackDate='DEFAULT_VAL'
+    const resolutionDate='DEFAULT_VAL'
+    const comments='DEFAULT_VAL'
+    const escalated='DEFAULT_VAL'
+  return (
+    <div className="flex border-dashed border-3 py-3 mt-3 h-70">
+      <div className=" w-[50%] flex flex-col gap-1 justify-center pl-2 border-r-3 border-dashed">
+        <div className="text-[18px]">
+          <span className="font-bold">Tracking ID: </span> {trackingID}
         </div>
-    );
+        <div className="text-[18px]">
+          <span className="font-bold">Current Status: </span> {status}
+        </div>
+        <div className="text-[18px]">
+          <span className="font-bold">Escalated?: </span> {escalated}
+        </div>
+        <div className="text-[18px]">
+          <span className="font-bold">Department: </span> {dept}
+        </div>
+        <div className="text-[18px]">
+          <span className="font-bold">Date of Acknowledgement: </span> {ackDate}
+        </div>
+        <div className="text-[18px]">
+          <span className="font-bold">Date of Resolution: </span> {resolutionDate}
+        </div>
+      </div>
+      <div className=" w-[50%] flex flex-col gap-1 px-2 overflow-y-scroll">
+        <div className="text-[18px] flex flex-col">
+          <p className="font-bold text-center">Comments from Department </p>
+          {comments} Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere alias laborum id labore ex quas provident. Officia dolorem facilis cupiditate aperiam, libero, mollitia, rerum expedita quisquam iure quo distinctio est! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores error nihil blanditiis culpa, quasi expedita optio ut quia magnam pariatur necessitatibus quibusdam doloribus cum. Nam voluptatibus officiis iusto nisi voluptas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci magnam doloribus fuga! Iusto, qui nemo, quae cupiditate sapiente perspiciatis ducimus totam nobis ullam atque expedita voluptatem reprehenderit non officia eum.
+        </div>
+      </div>
+    </div>
+  );
 }
 export default IssueAction;
--- frontend/src/components/IssueDetails.jsx
+++ frontend/src/components/IssueDetails.jsx
@@ -8,8 +8,8 @@ function IssueDetails() {
   const issueLocation = "DEFAULT_VAL";
   const issueDate = "DEFAULT_VAL";
   return (
-    <div className="flex border-dotted border-3 py-3 mt-3">
-      <div className=" w-[50%] flex flex-col gap-1 justify-center pl-2">
+    <div className="flex border-dashed border-3 py-3 mt-3 h-80">
+      <div className=" w-[50%] flex flex-col gap-1 justify-center pl-2 border-r-3 border-dashed">
         <div className="text-[18px]">
           <span className="font-bold">Tracking ID: </span> {issueID}
         </div>
@@ -20,8 +20,8 @@ function IssueDetails() {
         <div className="text-[18px]">
           <span className="font-bold">Issue Title: </span> {issueTitle}
         </div>
-        <div className="text-[18px]">
-          <span className="font-bold">Issue Description: </span> {issueDesc}
+        <div className="text-[18px] overflow-y-scroll flex flex-col mr-2 border-2 px-2">
+          <span className="font-bold">Issue Description: </span> {issueDesc} Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi minima reprehenderit quia nemo consequuntur eaque eius! Esse, ex necessitatibus eveniet, perferendis temporibus aut, sint commodi earum ducimus tempora dolore quasi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, quae a, doloribus harum dolores et sunt est suscipit delectus culpa quis dolorem deleniti perspiciatis ullam doloremque exercitationem? Molestias, optio ipsum? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias voluptas facere minus! Corrupti aut, mollitia nostrum eveniet fuga tenetur, nisi numquam voluptate illum consectetur iste explicabo rerum itaque ab debitis.
         </div>
         <div className="text-[18px]">
           <span className="font-bold">Issue Location: </span> {issueLocation}
