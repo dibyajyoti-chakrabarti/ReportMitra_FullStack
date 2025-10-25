85113bfea9b9dea9f5f53099245a6c7d2b041896
Adidev <anandadidev43@gmail.com>
2025-10-25 13:52:28 +0530

    Added aadhaar verification

--- backend/report_hub/urls.py
+++ backend/report_hub/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     path('api/reports/', include('report.urls')),
     path('api/profile/', include('user_profile.urls')),
+    path('api/aadhaar/', include('aadhaar.urls')),
     path('track/detail/<int:id>/', PublicIssueReportDetailView.as_view(), name='report-detail'),
 ]
